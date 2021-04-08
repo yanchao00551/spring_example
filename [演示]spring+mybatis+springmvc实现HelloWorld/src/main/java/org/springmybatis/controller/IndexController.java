bb5eb145859db73d39e1900f4fd31ee29498b864
yanchao00551 <10947@163.com>
2021-04-08 10:18:26 +0800

    案例提交

--- [演示]spring+mybatis+springmvc实现HelloWorld/src/main/java/org/springmybatis/controller/IndexController.java
+++ [演示]spring+mybatis+springmvc实现HelloWorld/src/main/java/org/springmybatis/controller/IndexController.java
@@ -1,6 +1,10 @@
 package org.springmybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springmybatis.service.UserService;
@@ -16,13 +20,29 @@ import javax.servlet.http.HttpServletResponse;
  * @date: 2021/4/7 15:32
  * @email: 10947@163.com
  */
-public class IndexController extends AbstractController {
 
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        System.out.println("hello,SpringMVC");
-        return new ModelAndView("index");
+/**
+ * 控制器默认单例，非线程安全
+ * @author 悟空
+ * @description //TODO
+ * @date 10:06 2021/4/8
+ * @param null
+ * @return null
+ */
+@RestController
+@RequestMapping("/home")
+public class IndexController {
+
+    private int i = 0;
+
+    @RequestMapping("/index")
+    public Integer index(){
+        return i++;
     }
 
-
+    @RequestMapping("/index2")
+    public Integer index2(){
+        return i++;
+    }
 }
+
